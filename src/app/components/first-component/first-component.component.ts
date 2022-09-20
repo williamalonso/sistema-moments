d57a705c92b3ebd6f473120889a123c9c913bc32
William Alonso <williamalonso@outlook.com>
2022-09-20 01:42:51 -0300

    :zap: initial commit

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css']
+})
+export class FirstComponentComponent implements OnInit {
+  
+  name: string = 'William'; // estamos explicitando o tipo string
+  age: number = 30;
+  hobbies = ['Correr', 'Jogar', 'Estudar'];
+  car = {
+    name: 'Polo',
+    year: 2019,
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
