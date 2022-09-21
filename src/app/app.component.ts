8dc9d0caea15344d3ca93a561c3bf9ad07eb689f
William Alonso <williamalonso@outlook.com>
2022-09-21 00:38:33 -0300

    :zap: learning directives and data sharing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  userName = 'Joaquim';
+
+  userData = {
+    email: 'joaquim@email.com',
+    role: 'Admin',
+  }
+
   title = 'moments';
 }
