8c30a5af4eaf068f0f80ccbc315ac90b9090d8af
William Alonso <williamalonso@outlook.com>
2022-09-23 21:14:40 -0300

    :zap: learning interfaces

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Animal } from 'src/app/interfaces/Animal';
 
 @Component({
   selector: 'app-list-render',
@@ -7,18 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListRenderComponent implements OnInit {
 
-  animals = [
-    {name: "Turca", type: "Dog"},
-    {name: "Tom", type: "Cat"},
-    {name: "Frida", type: "Dog"},
-    {name: "Bob", type: "Horse"},
+  animals: Animal[] = [
+    { name: "Turca", type: "Dog", age: 4 },
+    { name: "Tom", type: "Cat", age: 10 },
+    { name: "Frida", type: "Dog", age: 5 },
+    { name: "Bob", type: "Horse", age: 1 },
   ];
 
   class = ["class"];
 
+  animalDetails = '';
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log("teste");
+  }
+
+  showAge(animal: Animal) {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos`;
   }
 
 }
