800523b5609286d482cba73871d3fe455b304711
William Alonso <williamalonso@outlook.com>
2022-09-25 00:22:11 -0300

    :zap: learning services

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Animal } from 'src/app/interfaces/Animal';
+import { ListService } from 'src/app/services/list.service';
 
 @Component({
   selector: 'app-list-render',
@@ -19,14 +20,18 @@ export class ListRenderComponent implements OnInit {
 
   animalDetails = '';
 
-  constructor() { }
+  constructor(private listService: ListService) { }
 
   ngOnInit(): void {
-    console.log("teste");
   }
 
   showAge(animal: Animal) {
     this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos`;
+  }
+
+  removeAnimal(animal: Animal) {
+    console.log("removendo animal...");
+    this.animals = this.listService.remove(this.animals, animal);
   }
 
 }
