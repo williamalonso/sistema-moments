b882d13ba69d72e37826fdad8efb167959a55da0
William Alonso <williamalonso@outlook.com>
2022-10-04 01:50:26 -0300

    :zap: preparing project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./components/pages/home/home.component";
+import { AboutComponent } from "./components/pages/about/about.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
     declarations: [],
